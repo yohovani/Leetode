608f3beedfe5f1912e311033d04797fe2d81a007
yohovani vargas <yohovanivargas@gmail.com>
2025-07-16 18:16:27 -0600

    https://leetcode.com/problems/reconstruct-original-digits-from-english/submissions/1700669228/

--- ReconstructOriginalDigits.java
+++ ReconstructOriginalDigits.java
@@ -14,13 +14,18 @@ public class ReconstructOriginalDigits {
         }
 
         public String constructNumber(String number, String stri){
+            String aux = stri;
             for(int j = 0;j<number.length();j++){
                 int search = stri.indexOf(number.charAt(j));
                 if(search != -1){
                     stri = remove_character(stri, search);
                 }
             }
-            return stri;
+            if(stri.length() + number.length() == aux.length()){
+                return stri;
+            }else{
+                return aux;
+            }
         }
 
         public String originalDigits(String s) {
@@ -40,75 +45,96 @@ public class ReconstructOriginalDigits {
         
         for(int i=0;i<letter_filter.length();i++){
             int search = s.indexOf(letter_filter.charAt(i));
+            String aux = s;
             if(search != -1){
                 switch(letter_filter.charAt(i)){
                     case 'z':{
                         number = "zero";
                         s = constructNumber(number,s);
-                        numbers.add(0);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(0);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'w':{
                         number = "two";
                         s = constructNumber(number,s);
-                        numbers.add(2);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(2);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'x':{
                         number = "six";
                         s = constructNumber(number,s);
-                        numbers.add(6);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(6);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'f':{
                         number = "five";
                         s = constructNumber(number,s);
-                        numbers.add(5);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(5);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'u':{
                         number = "four";
                         s = constructNumber(number,s);
-                        numbers.add(4);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(4);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 's':{
                         number = "seven";
                         s = constructNumber(number,s);
-                        numbers.add(7);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(7);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 't':{
                         number = "three";
                         s = constructNumber(number,s);
-                        numbers.add(3);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(3);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'g':{
                         number = "eight";
                         s = constructNumber(number,s);
-                        numbers.add(8);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(8);
+                        }
                         System.out.println(s);
                         break;
                     }
                     case 'o':{
                         number = "one";
                         s = constructNumber(number,s);
-                        numbers.add(1);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(1);
+                        }
                         System.out.println(s);
                         break;
                     }
                     default:{
                         number = "nine";
                         s = constructNumber(number,s);
-                        numbers.add(9);
+                        if(number.length() + s.length() == aux.length()){
+                            numbers.add(9);
+                        }
                         System.out.println(s);
                         break;
                     }
